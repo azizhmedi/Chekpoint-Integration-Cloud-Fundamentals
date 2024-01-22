ee1f89bbadbb3fa37f4eccf0868c0ac13685710b
azizhmedi <zizouhmedi1213@gmail.com>
2024-01-22 15:33:00 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import { Marker, Popup, MapContainer, TileLayer } from "react-leaflet";
+import L from "leaflet";
+import "leaflet-control-geocoder/dist/Control.Geocoder.css";
+import "leaflet-control-geocoder/dist/Control.Geocoder.js";
+import LeafletGeocoder from "./LeafletGeocoder";
+
+function App() {
+  const position = [36.787606, 10.113046];
+  return (
+    <div className="App">
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <LeafletGeocoder />
+      </MapContainer>
+    </div>
+  );
+}
+let DefaultIcon = L.icon({
+  iconUrl: "./icon.png",
+  iconSize: [25, 31],
+});
+L.Marker.prototype.options.icon = DefaultIcon;
+export default App;
